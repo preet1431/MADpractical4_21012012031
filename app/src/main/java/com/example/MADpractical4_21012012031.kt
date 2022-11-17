279fcbb90de6405b9fa194b1742dde68abb087a7
preet1431 <110801380+preet1431@users.noreply.github.com>
2022-11-18 04:47:18 +0530

    Initial commit

---
+++ app/src/main/java/com/example/MADpractical4_21012012031.kt
@@ -0,0 +1,5 @@
+package com.example
+
+class MADpractical4_21012012031 {
+
+}
---
+++ app/src/main/java/com/example/madpractical4_21012012031/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.madpractical4_21012012031;
+
+import android.app.Activity;
+
+public class MainActivity extends Activity {
+}
